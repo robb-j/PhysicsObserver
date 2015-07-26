a888b4f36e272276a11fecfa48528ecef68c7793
Robert Anderson <robster31@googlemail.com>
2015-07-26 18:52:41 +0100

    + Added a way of removing all observations for a given SKNode + Added default 568 launch image

--- PhysicsObserver/Observer/ContactTypes.swift
+++ PhysicsObserver/Observer/ContactTypes.swift
@@ -29,10 +29,19 @@ typealias ContactObservation = (contact: SKPhysicsContact, phase: ContactPhase) 
 /// A type of contact to be used when removing an observation
 enum ContactType {
 	
+	/// A Contact between two categories
 	case Category(UInt32, UInt32)
+	
+	/// A Contact between an SKNode and a category
 	case NodeCategory(SKNode, UInt32)
+	
+	/// A Contact between two SKNodes
 	case Node(SKNode, SKNode)
+	
+	/// Any contact involving this SKNode
+	case OnNode(SKNode)
 }
+
 
 
 
@@ -42,20 +51,42 @@ func == (contact: ContactObserver, type: ContactType) -> Bool {
 	
 	switch type {
 		
+		
 	case .Category(let catA, let catB) where contact is CategoryContactObserver:
 		
 		let cc = contact as! CategoryContactObserver
 		return cc.categoryA == cc.categoryA && cc.categoryB == cc.categoryB
 		
+		
+		
 	case .NodeCategory(let node, let cat) where contact is NodeCategoryContactObserver:
 		
 		let nc = contact as! NodeCategoryContactObserver
-		return nc.node === node && nc.category == cat		
+		return nc.node === node && nc.category == cat
+		
+		
 		
 	case .Node(let nodeA, let nodeB) where contact is NodeContactObserver:
 		
 		let nn = contact as! NodeContactObserver
 		return nn.nodeA == nodeA && nn.nodeB == nodeB
+		
+		
+		
+	case .OnNode(let node):
+		
+		if let nn = contact as? NodeContactObserver {
+			
+			return nn.nodeA == node || nn.nodeB == node
+		}
+		else if let nc = contact as? NodeCategoryContactObserver {
+			
+			return nc.node == node
+		}
+		else {
+			
+			return false
+		}
 		
 	default:
 		return false
